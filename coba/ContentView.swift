9b4f97a6480e8434b166ab6bbd5be803555ef01f
Jeremy Ernesto <jeremyernestosw@gmail.com>
2023-06-21 09:13:18 +0700

    Initial Commit

---
+++ coba/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  coba
+//
+//  Created by Jeremy Ernesto Sean Wahono on 21/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, madafaka!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coba/cobaApp.swift
@@ -0,0 +1,17 @@
+//
+//  cobaApp.swift
+//  coba
+//
+//  Created by Jeremy Ernesto Sean Wahono on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct cobaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
